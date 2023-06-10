18363d815f5c74b466ca1969798d416290296632
fahmiabdulyakub <fahmiabdulyakub28@gmail.com>
2023-06-10 14:22:41 +0700

    create unsplash service

---
+++ src/services/UnsplashService.ts
@@ -0,0 +1,23 @@
+import {IPhoto} from '@types';
+import axios from 'axios';
+import Config from 'react-native-config';
+
+export const searchPhotos = (query: string, page: number) => {
+  return new Promise<IPhoto[]>(resolve => {
+    axios
+      .get(`${Config.BASE_URL}search/photos`, {
+        params: {
+          query,
+          client_id: Config.API_KEY,
+          page,
+          per_page: 10,
+        },
+      })
+      .then(result => {
+        resolve(result.data.results);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  });
+};
